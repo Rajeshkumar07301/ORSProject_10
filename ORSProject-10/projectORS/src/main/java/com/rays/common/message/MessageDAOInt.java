2d458590789e48ce7d44596fae3317467162140c
Rajeshkumar07301 <rajeshsakwar40@gmail.com>
2024-01-13 22:46:50 +0530

    donr

---
+++ ORSProject-10/projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Rajesh Kumar
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
